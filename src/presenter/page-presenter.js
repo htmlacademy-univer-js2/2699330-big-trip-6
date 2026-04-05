ac3b380ca2e366f1bf36e6d72e0f3d837deddbeb
Tatiana Inozemtseva <insmzewa@icloud.com>
2026-04-05 22:56:12 +0500

    Шаблонизируй это

---
+++ src/presenter/page-presenter.js
@@ -0,0 +1,38 @@
+import FilterView from '../view/filter-view.js';
+import Sorting from '../view/sorting-view.js';
+import EditPointView from '../view/edit-event-form-view.js';
+import PointView from '../view/point-view.js';
+import TripPoint from '../view/trip-point-view.js';
+import PointListView from '../view/point-list-view.js';
+import { render, RenderPosition } from '../render.js';
+
+export default class TripPresenter {
+  constructor(tripModel) {
+    this.model = tripModel;
+
+    this.filtersContainer = document.querySelector('.trip-controls__filters');
+    this.eventsContainer = document.querySelector('.trip-events');
+    this.mainContainer = document.querySelector('.trip-main');
+  }
+
+  init() {
+    const { points, destinations, offers } = this.model;
+
+    render(new TripPoint(), this.mainContainer, RenderPosition.AFTERBEGIN);
+    render(new FilterView(), this.filtersContainer);
+    render(new Sorting(), this.eventsContainer, RenderPosition.AFTERBEGIN);
+
+    const pointListView = new PointListView();
+    render(pointListView, this.eventsContainer);
+    const pointsListContainer = pointListView.getElement();
+
+    const edPointView = new EditPointView({point: points[0], destinations, offers});
+    render(edPointView, pointsListContainer);
+
+    points.forEach((point) => {
+      const pointView = new PointView({point, destinations, offers});
+      render(pointView, pointsListContainer);
+    });
+  }
+}
+
